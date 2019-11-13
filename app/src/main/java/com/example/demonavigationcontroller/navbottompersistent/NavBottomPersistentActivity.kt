ce4cc40b477ac70bc70df02b9d22b72ea459ea5c
Pasztor Arthur <arthur.pasztor@softvision.ro>
2019-11-13 12:05:29 +0200

    Presentation

--- app/src/main/java/com/example/demonavigationcontroller/navbottompersistent/NavBottomPersistentActivity.kt
+++ app/src/main/java/com/example/demonavigationcontroller/navbottompersistent/NavBottomPersistentActivity.kt
@@ -10,22 +10,9 @@ class NavBottomPersistentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_bottom_persistent)
-        if (savedInstanceState == null) {
-            setupBottomNavigationBar()
-        } // Else, need to wait for onRestoreInstanceState
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        super.onRestoreInstanceState(savedInstanceState)
-        // Now that BottomNavigationBar has restored its instance state
-        // and its selectedItemId, we can proceed with setting up the
-        // BottomNavigationBar with Navigation
         setupBottomNavigationBar()
     }
 
-    /**
-     * Called on first creation and when restoring state.
-     */
     private fun setupBottomNavigationBar() {
         val navGraphIds = listOf(
             R.navigation.nav_bottom_persistent_home,
@@ -33,8 +20,7 @@ class NavBottomPersistentActivity : AppCompatActivity() {
             R.navigation.nav_bottom_persistent_album
         )
 
-        // Setup the bottom navigation view with a list of navigation graphs
-        val controller = bottom_nav_view.setupWithNavController(
+        bottom_nav_view.setupWithNavController(
             navGraphIds = navGraphIds,
             fragmentManager = supportFragmentManager,
             containerId = R.id.nav_host_container,
